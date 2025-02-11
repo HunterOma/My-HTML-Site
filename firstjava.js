9f5e6db48488f4bf8ea7964f43cd18957e8f4595
HunterOma <142925776+HunterOma@users.noreply.github.com>
2025-02-11 12:35:05 -0700

    Update firstjava.js

--- firstjava.js
+++ firstjava.js
@@ -1,5 +1,3 @@
 function testFunction() {
     document.getElementById("paragraph").innerHTML = "Hunter, Web Design /Web App Development 2";
-    let z = 5 * 9;
-document.getElementById("math").innerHTML = z;
   }
